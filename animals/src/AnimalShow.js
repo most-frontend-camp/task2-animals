be0a5696c1eff32902c6851111fc042254b344b2
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-29 06:28:15 +0500

    add some likes

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
 import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
 import heart from './svg/heart.svg';
 import horse from './svg/horse.svg';
 
@@ -10,13 +12,21 @@ const svgMap = {
     cat,
     cow,
     dog,
-    horse
+    gator,
+    horse,
 };
 
-function AnimalShow({type}) {
+function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
     return (
-        <div>
+        <div onClick={handleClick}>
             <img alt="animal" src={svgMap[type]} />
+            <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks + 'px' }} />
         </div>
     );
 }
