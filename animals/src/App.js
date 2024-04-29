31c5d9484821633b05d5398588d6d24048ba093a
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-29 06:43:02 +0500

    css applied

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState } from 'react';
 import Animals from './AnimalShow';
 import AnimalShow from "./AnimalShow";
@@ -23,9 +24,9 @@ function App() {
     });
 
     return (
-        <div>
+        <div className="app">
             <button onClick={handleClick}>Add animal</button>
-        <div>{renderedAnimals}</div>
+        <div className="animal-list">{renderedAnimals}</div>
     </div>
     );
 }
