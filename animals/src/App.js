bfbc918689f2f67b25c728fff2a65007c78ede89
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-29 05:38:12 +0500

    updated onClick and getRandomAnimal

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,10 +1,23 @@
+import {useState} from 'react';
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+console.log(getRandomAnimal());
+
 function App() {
+    const [count, setCount] = useState(0);
     const handleClick = () => {
-      console.log('Мы нажали на кнопку!');
+      // console.log('Мы нажали на кнопку!');
+        setCount(count + 1);
     };
 
     return <div>
     <button onClick={handleClick}>Add animal</button>
+        <div>Число животных будет {count}</div>
     </div>;
 }
 
